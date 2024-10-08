03ac0b1d6a5318c0f2390ef25aceec29676f2906
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2024-10-08 20:06:44 +0300

    Add edit, update, destroy action for articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,6 +21,25 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to @article
+  end
+
   def article_params
     params.require(:article).permit(:title,:body)
   end
