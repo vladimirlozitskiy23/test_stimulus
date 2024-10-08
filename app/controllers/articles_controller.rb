9a0a730366bd3461d16d070db6882d3ecb12c339
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2024-10-08 17:50:45 +0300

    add show and create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,21 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
+  end
+
+  def article_params
+    params.require(:article).permit(:title,:body)
+  end
 end
